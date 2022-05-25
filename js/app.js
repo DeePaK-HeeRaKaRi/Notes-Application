e7a5fe75e4e301f74126f9026b231946229e6978
DeePaK-HeeRaKaRi <dkh9558@gmail.com>
2022-05-25 19:00:20 +0530

    first commit

--- js/app.js
+++ js/app.js
@@ -6,43 +6,51 @@ addBtn.addEventListener("click", function (e) {
   let addTitle=document.getElementById("addTitle")
   // console.log("title", addTitle)
   let addTxt = document.getElementById("addTxt");
-  let todayDate=new Date()
-  let notes = localStorage.getItem("notes");
-  if (notes == null) {
-    notesObj = [];
-  } else {
-    notesObj = JSON.parse(notes);
+  if(addTitle.value.length >5 && addTxt.value.length>5){
+    let todayDate=new Date()
+    let notes = localStorage.getItem("notes");
+    if (notes == null) {
+      notesObj = [];
+    }
+    // } else { 
+    //   notesObj = JSON.parse(notes);
 
-    console.log("parse", typeof notesObj, notesObj);
+    //   console.log("parse", typeof notesObj, notesObj);
+    // }
+    let myObj={
+      title:addTitle.value,
+      text:addTxt.value,
+      date:`${todayDate.getDate()}-${todayDate.getMonth()}-${todayDate.getFullYear()} ${todayDate.getHours()}:${todayDate.getMinutes()}:${todayDate.getSeconds()}`,
+    
+    } 
+
+    notesObj.push(myObj);
+    // Array of objects
+    localStorage.setItem("notes", JSON.stringify(notesObj));
+    // document.getElementById('setItem').innerHTML=addTxt.value
+    addTxt.value = "";
+    addTitle.value=''
+
+    console.log("stringify", typeof notesObj, notesObj);
+    showNotes();
+  }else{
+    alert('Length should be minimum of 6')
+    addTxt.value = "";
+    addTitle.value=''
   }
-  let myObj={
-    title:addTitle.value,
-    text:addTxt.value,
-    date:`${todayDate.getDate()}-${todayDate.getMonth()}-${todayDate.getFullYear()} ${todayDate.getHours()}:${todayDate.getMinutes()}:${todayDate.getSeconds()}`,
   
-  }
-
-  notesObj.push(myObj);
-  // Array of objects
-  localStorage.setItem("notes", JSON.stringify(notesObj));
-  // document.getElementById('setItem').innerHTML=addTxt.value
-  addTxt.value = "";
-  addTitle.value=''
-
-  console.log("stringify", typeof notesObj, notesObj);
-  showNotes();
 });
  
-
+ 
 function showNotes() {
   let notes = localStorage.getItem("notes");
-  if (notes == null) {
-    notesObj = [];
-  } else {
-    notesObj = JSON.parse(notes);
-
-    // console.log('parse',typeof notesObj,notesObj)
-  }
+  // if (notes == null) {
+  //   notesObj = [];
+  // } else {
+  //   notesObj = JSON.parse(notes);
+  //   console.log('parse',typeof notesObj,notesObj)
+  // }
+  notesObj = JSON.parse(notes);
   let html = "";
   notesObj.forEach(function (element, index) {
     html += `
@@ -52,7 +60,7 @@ function showNotes() {
                   <p class="card-text" id="setItem">${element.text}</p>
                   <br>
                   <p class="card-text">Created At :- ${element.date}</p>
-                  <button id="${index}" onclick="deleteNote(this.id)" class="btn btn-primary">Delete Note</button>
+                  <button id="${index}" onclick="deleteNote(${index})" class="btn btn-primary">Delete Note</button>
                 </div>
             </div>
         `;
@@ -66,14 +74,26 @@ function showNotes() {
 function deleteNote(indexx) {
   console.log("im deleting", indexx);
   let notes = localStorage.getItem("notes");
-  if (notes == null) {
-    notesObj = [];
-  } else {
-    notesObj = JSON.parse(notes);
+  // if (notes == null) {
+  //   notesObj = [];
+  // } else {
+  //   notesObj = JSON.parse(notes);
+  // }
+  notesObj = JSON.parse(notes);
+  if(notesObj.length==1){
+    notesObj.pop()
+    // notesObj.shift()
+    localStorage.setItem("notes", JSON.stringify(notesObj));
+    location.reload()
+   
+    showNotes();
+  }else{
+    notesObj.splice(indexx, 1);
+    localStorage.setItem("notes", JSON.stringify(notesObj));
+    showNotes();
   }
-  notesObj.splice(indexx, 1);
-  localStorage.setItem("notes", JSON.stringify(notesObj));
-  showNotes();
+  
+  
 }
 
 
@@ -83,7 +103,7 @@ searchTxt.addEventListener("input", function (e) {
   console.log("Input event fired", inputVal);
   let noteCards = document.getElementsByClassName("noteCard");
   Array.from(noteCards).forEach(function (e) {
-    let cardTxt = e.getElementsByTagName("p")[0].innerHTML;
+    let cardTxt = e.getElementsByTagName("h5")[0].innerHTML;
     // console.log(cardTxt)
     if (cardTxt.toLowerCase().includes(inputVal.toLowerCase())) {
       e.style.display = "block";
